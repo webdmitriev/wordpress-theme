7f0e240f045fb3987f97717ea10c97f642d094fe
webdmitriev <webdmitriev@gmail.com>
2025-10-31 16:48:00 +0300

    added page template for development

---
+++ pages/page-development.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Development
+*/
+
+
+get_header();
+
+$url = get_template_directory_uri();
+$image_base64 = 'data:image/gif;base64,R0lGODlhBwAFAIAAAP///wAAACH5BAEAAAEALAAAAAAHAAUAAAIFjI+puwUAOw==';
+?>
+
+
+<?php
+get_footer();
